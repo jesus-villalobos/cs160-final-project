a905c561c2c413b375bf8b0f068744fa337f6fe5
Jesus Villalobos <jvillalobos@berkeley.edu>
2024-04-16 05:38:43 -0700

    added initial chat stuff

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import "./../App.css";
 
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+    MainContainer,
+    ChatContainer,
+    MessageList,
+    Message,
+    MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+
+import "react-chat-elements/dist/main.css";
+
 import MainAppFormat from "./../components/MainAppLayout";
 
 const ChatPage: React.FC = () => {
     return (
         <div className="AppLayoutContainer">
-            <MainAppFormat pageBody={<></>} />
+            <MainAppFormat
+                pageBody={
+                    <div className="ChatPageBody">
+                        <Message
+                            model={{
+                                message: "Hello my friend",
+                                sender: "Joe",
+                                sentTime: "just now",
+                                direction: "incoming",
+                                position: "single",
+                            }}
+                        />
+                    </div>
+                }
+            />
         </div>
     );
 };
